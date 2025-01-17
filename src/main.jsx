9a9fc006d435c367820b1fbd3d1e84485c1bab8b
jonorl <39122230+jonorl@users.noreply.github.com>
2025-01-17 16:13:05 +0300

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Salutation from './Greeting.jsx'
+import { DisplayChocotorta } from './Chocotorta.jsx'
+import './index.css'
+import App from './Animals.jsx'
+import Buttons from './Buttons.jsx'
+import PackingList from './Packed.jsx'
+import Person from './Person.jsx'
+import Component from './Count.jsx'
+import CustomInput from './CustomInput.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <CustomInput />
+    <Component />
+    <Person />
+    <DisplayChocotorta />
+    <Salutation />
+    <App />
+    <PackingList />
+    <Buttons />
+  </StrictMode>,
+)
