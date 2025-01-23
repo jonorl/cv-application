9d0e6745c1d994f42c42a097ab746a4172d16884
jonorl <39122230+jonorl@users.noreply.github.com>
2025-01-23 12:00:09 +0300

    Feat: Added submit button on Preview with submission message

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,18 +1,38 @@
+import { useState } from 'react'
+
 export default function Preview({ generalInfo, educationInfo, experienceInfo }) {
-    return <div className="preview">
-        <h1>{generalInfo.fname} {generalInfo.lname}</h1>
-        <h2>{generalInfo.email}</h2>
-        <h2>{generalInfo.phone}</h2>
-        <br />
-        <h1>{educationInfo.school}</h1>
-        <h2>{educationInfo.title}</h2>
-        <h2>{educationInfo.date}</h2>
-        <br />
-        <h1>{experienceInfo.company}</h1>
-        <h2>{experienceInfo.position}</h2>
-        <h2>{experienceInfo.responsibilities}</h2>
-        <h2>{experienceInfo.dateFrom}</h2>
-        <h2>{experienceInfo.dateTo}</h2>
+
+    const [isSubmitted, setSubmitted] = useState(false);
+
+    if (isSubmitted) {
+        return <h2 className='submitted'>Your Application has been submitted!</h2>
+      }
+
+    function handleSubmit() {
+        setSubmitted(true)
+    }
+
+    return <div className="preview-container">
+
+        <div className="preview">
+            <h1>{generalInfo.fname} {generalInfo.lname}</h1>
+            <h2>{generalInfo.email}</h2>
+            <h2>{generalInfo.phone}</h2>
+            <br />
+            <h1>{educationInfo.school}</h1>
+            <h2>{educationInfo.title}</h2>
+            <h2>{educationInfo.date}</h2>
+            <br />
+            <h1>{experienceInfo.company}</h1>
+            <h2>{experienceInfo.position}</h2>
+            <h2>{experienceInfo.responsibilities}</h2>
+            <h2>{experienceInfo.dateFrom}</h2>
+            <h2>{experienceInfo.dateTo}</h2>
+        </div>
+
+        <button className="submit" onClick={() => handleSubmit()}>Submit</button>
     </div>
+
+
 
 }
